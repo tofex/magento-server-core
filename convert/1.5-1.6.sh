e38c6cd61eff23660a3c65d25c34ae082632cfaf
Andreas Knollmann <aknollmann@tofex.de>
2022-05-13 17:03:30 +0200

    added moving of install

--- convert/1.5-1.6.sh
+++ convert/1.5-1.6.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+versionCompare() {
+  if [[ "$1" == "$2" ]]; then
+    echo "0"
+  elif [[ "$1" = $(echo -e "$1\n$2" | sort -V | head -n1) ]]; then
+    echo "1"
+  else
+    echo "2"
+  fi
+}
+
 currentPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -f "${currentPath}/../env.properties" ]; then
@@ -34,6 +44,22 @@ if [[ -n "${servers}" ]]; then
     fi
   done
   ini-del "${currentPath}/../env.properties" "project" "servers"
+fi
+
+echo "Moving install"
+magentoVersion=$(ini-parse "${currentPath}/../env.properties" "no" "project" "magentoVersion")
+if [[ -n "${magentoVersion}" ]]; then
+  composerUser=$(ini-parse "${currentPath}/../env.properties" "no" "project" "composerUser")
+  composerPassword=$(ini-parse "${currentPath}/../env.properties" "no" "project" "composerPassword")
+  if [[ $(versionCompare "${magentoVersion}" "1.9.4.5") == 0 ]] || [[ $(versionCompare "${magentoVersion}" "1.9.4.5") == 1 ]]; then
+    composerServer="https://composer.tofex.de"
+  else
+    composerServer="https://repo.magento.com"
+  fi
+  ini-set "${currentPath}/../env.properties" yes install repositories "composer|${composerServer}|${composerUser}|${composerPassword}"
+  ini-move "${currentPath}/../env.properties" "yes" "project" "magentoVersion" "install" "magentoVersion"
+  ini-move "${currentPath}/../env.properties" "yes" "project" "magentoEdition" "install" "magentoEdition"
+  ini-move "${currentPath}/../env.properties" "yes" "project" "mageMode" "install" "magentoMode"
 fi
 
 echo "Moving hosts"
