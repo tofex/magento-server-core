74b003d0ce49bd72935a52f02df8b503a59a8631
Andreas Knollmann <aknollmann@tofex.de>
2022-05-23 19:55:14 +0200

    added update script

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+scriptName="${0##*/}"
+
+usage()
+{
+cat >&2 << EOF
+usage: ${scriptName} options
+
+OPTIONS:
+  -h  Show this message
+
+Example: ${scriptName}
+EOF
+}
+
+trim()
+{
+  echo -n "$1" | xargs
+}
+
+while getopts hs:? option; do
+  case "${option}" in
+    h) usage; exit 1;;
+    ?) usage; exit 1;;
+  esac
+done
+
+currentPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${currentPath}/.."
+
+source <(curl -s -L https://bitbucket.org/tofex/magento-server-create/raw/master/update.sh)
