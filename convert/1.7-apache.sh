a96fc8aec9fd1991d2208dc3c3472fb3dffe2190
Andreas Knollmann <aknollmann@tofex.de>
2022-06-24 06:59:06 +0200

    added update of apache web server

--- convert/1.7-apache.sh
+++ convert/1.7-apache.sh
@@ -9,49 +9,12 @@ fi
 
 cd "${currentPath}"
 
-webServer=$("${currentPath}/../server/web-server/single.sh")
+webServers=( $("${currentPath}/../server/web-server/all.sh") )
 
-if [[ -n "${webServer}" ]]; then
+for webServer in "${webServers[@]}"; do
   type=$(ini-parse "${currentPath}/../../env.properties" "no" "${webServer}" "type")
 
   if [[ "${type}" == "apache" ]]; then
-
+    "${currentPath}/../../env/update-web-server.sh" -i "${webServer}" -t "apache_php"
   fi
-fi
-
-apacheHost=$(ini-parse "${currentPath}/../../env.properties" "no" "project" "apacheHost")
-if [[ -n "${apacheHost}" ]]; then
-  echo "Moving Apache"
-  servers=$(ini-parse "${currentPath}/../../env.properties" "no" "project" "servers")
-  if [[ -n "${servers}" ]]; then
-    IFS=',' read -r -a serverList <<< "${servers}"
-  else
-    serverList=( $(ini-parse "${currentPath}/../../env.properties" "yes" "system" "server") )
-  fi
-  serverFound=0
-  for server in "${serverList[@]}"; do
-    type=$(ini-parse "${currentPath}/../../env.properties" "yes" "${server}" "type")
-    if [[ "${type}" == "local" ]]; then
-      if [[ "${apacheHost}" == "localhost" ]] || [[ "${apacheHost}" == "127.0.0.1" ]]; then
-        ini-set "${currentPath}/../../env.properties" "yes" "${server}" "webServer" "web_server"
-        serverFound=1
-      fi
-    elif [[ "${type}" == "ssh" ]]; then
-      sshHost=$(ini-parse "${currentPath}/../../env.properties" "yes" "${server}" "sshHost")
-      if [[ "${sshHost}" == "${apacheHost}" ]]; then
-        ini-set "${currentPath}/../../env.properties" "yes" "${server}" "webServer" "web_server"
-        serverFound=1
-      fi
-    fi
-  done
-  if [[ "${serverFound}" == 0 ]]; then
-    ini-set "${currentPath}/../../env.properties" "yes" "${apacheHost}" "webServer" "web_server"
-  fi
-  ini-del "${currentPath}/../../env.properties" "project" "apacheHost"
-  ini-set "${currentPath}/../../env.properties" "yes" "web_server" "type" "apache"
-  ini-move "${currentPath}/../../env.properties" "yes" "project" "apacheVersion" "web_server" "version"
-  ini-move "${currentPath}/../../env.properties" "yes" "project" "apacheHttpPort" "web_server" "httpPort"
-  ini-move "${currentPath}/../../env.properties" "yes" "project" "apacheSslPort" "web_server" "sslPort"
-else
-  echo "No Apache to move"
-fi
+done
