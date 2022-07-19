bb5e9382cbc9ba312efc1b8af34c951660bef209
Andreas Knollmann <aknollmann@tofex.de>
2022-07-19 15:30:12 +0200

    update parsing

--- prepare-parameters.sh
+++ prepare-parameters.sh
@@ -6,6 +6,15 @@ while [[ "$#" -gt 0 ]]; do
   parameter="${1}"
   shift
   if [[ "${parameter:0:2}" == "--" ]] || [[ "${parameter}" =~ ^-[[:alpha:]][[:space:]]* ]] || [[ "${parameter}" =~ ^-\?$ ]]; then
+    if [[ "${parameter}" =~ ^--[[:alpha:]]+[[:space:]]* ]]; then
+      parameter="${parameter:2}"
+      key=$(echo "${parameter}" | grep -oP '[[:alpha:]]+(?=\s)' | tr -d "\n")
+      value=$(echo "${parameter:${#key}}" | xargs)
+      # shellcheck disable=SC2034
+      parameters["${key}"]="${value}"
+      eval "${key}=\"${value}\""
+      continue
+    fi
     if [[ "${parameter:0:2}" == "--" ]]; then
       key="${parameter:2}"
     elif [[ "${parameter}" =~ ^-\?$ ]]; then
@@ -18,12 +27,12 @@ while [[ "$#" -gt 0 ]]; do
       eval "${key}=1"
     else
       value="${1}"
+      shift
       if [[ "${value:0:2}" == "--" ]]; then
         parameters["${key}"]=1
         eval "${key}=1"
         continue
       fi
-      shift
       # shellcheck disable=SC2034
       parameters["${key}"]="${value}"
       eval "${key}=\"${value}\""
